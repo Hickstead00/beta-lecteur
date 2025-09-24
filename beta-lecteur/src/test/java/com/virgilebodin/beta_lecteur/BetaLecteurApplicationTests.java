28ad47debf9227829847f138f8c5ec450ff334a2
Virgile <virgile.bodin@etu.univ-orleans.fr>
2025-09-24 19:46:45 +0200

    Initial commit with Spring boot back end and dependencies and boilerplate files

---
+++ beta-lecteur/src/test/java/com/virgilebodin/beta_lecteur/BetaLecteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virgilebodin.beta_lecteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaLecteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
